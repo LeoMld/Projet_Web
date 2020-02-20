ee0a9c6e341138627f0a2f0040c1473ac41b203b
leo <lele.mollard@gmail.com>
2020-02-20 21:14:26 +0100

    test commit with webstorm

--- app.js
+++ app.js
@@ -26,13 +26,14 @@ app.use('/', catalogRouter);
 
 
 
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
