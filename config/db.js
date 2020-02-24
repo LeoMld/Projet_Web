966acf75d3f474d95138d2d55d1e95f94c444b9a
leo <lele.mollard@gmail.com>
2020-02-24 17:35:52 +0100

    heroku db deploy

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
     host: 'eu-cdbr-west-02.cleardb.net',
     user: 'bfe83e2534b01b',
     password: '262d60fb',
-    database: 'Projet_Web'
+    database: 'heroku_c5894d609f3f5f9'
 });
 connection.connect();
 
