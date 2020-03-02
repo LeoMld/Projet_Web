4519b2dc409ff2458ea5cfac68ccb0a2a60aae79
leo <lele.mollard@gmail.com>
2020-03-02 13:18:16 +0100

    heroku

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql');
 
-const connection = mysql.createPool(process.env.CLEARDB_DATABASE_URL);
+const connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
 /*const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
