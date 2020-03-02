fbc7f3d7c7a43e4acd6611385126c69ee046065e
leo <lele.mollard@gmail.com>
2020-03-02 18:36:15 +0100

    heroku

--- config/db.js
+++ config/db.js
@@ -1,18 +1,18 @@
 const mysql = require('mysql');
 
-/*const connection = mysql.createPool(process.env.CLEARDB_DATABASE_URL);*/
-const connection = mysql.createPool({
+const connection = mysql.createPool(process.env.CLEARDB_DATABASE_URL);
+/*const connection = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: '',
     database: 'Projet_Web'
-});
+});*/
 
 connection.getConnection((err, connection) => {
     if(err)
-        console.error("Something went wrong connecting to the database ...")
+        console.error("Something went wrong connecting to the database ...");
     if(connection)
-        connection.release()
+        connection.release();
     return;
 });
 
